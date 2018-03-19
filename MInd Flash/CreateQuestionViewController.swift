31f621ea161ee06751d6c8f6acc73f9ddab522b2
David Rademaker <davidrademaker@Interapts-MacBook-Pro-97.local>
2018-03-19 16:38:31 -0400

    created ui

---
+++ MInd Flash/CreateQuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateQuestionViewController.swift
+//  MInd Flash
+//
+//  Created by David Rademaker on 3/19/18.
+//  Copyright © 2018 David Rademaker. All rights reserved.
+//
+
+import UIKit
+
+class CreateQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    
+
+}
