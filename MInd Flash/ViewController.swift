e79ea145f1b5e4a0dea94c35d8ee24a0d4ef5cf7
David Rademaker <davidrademaker@Interapts-MacBook-Pro-97.local>
2018-03-19 13:39:03 -0400

    Set up project files

---
+++ MInd Flash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MInd Flash
+//
+//  Created by David Rademaker on 3/19/18.
+//  Copyright © 2018 David Rademaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
